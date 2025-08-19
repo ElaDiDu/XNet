78c48ce2dcecc50c297797cb74e7b140edfca33c
ElaDiDu <50326418+ElaDiDu@users.noreply.github.com>
2025-08-20 00:00:40 +0300

    versioning 1.8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 // Project properties
 group = "eladidu.xnet"
-version = "1.8.4"
+version = "1.8.5"
 
 // Set the toolchain version to decouple the Java we run Gradle with from the Java used to compile and run the mod
 java {
