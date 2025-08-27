f55534c517b33dce74d0799a0a8658aa83ffbb88
ElaDiDu <50326418+ElaDiDu@users.noreply.github.com>
2025-08-27 13:39:45 +0300

    1.8.14 bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 // Project properties
 group = "eladidu.xnet"
-version = "1.8.13"
+version = "1.8.14"
 
 // Set the toolchain version to decouple the Java we run Gradle with from the Java used to compile and run the mod
 java {
