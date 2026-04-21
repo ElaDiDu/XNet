f0df408212a87296806b51df607d520211ff7faf
kaduvill <jottafanftw@gmail.com>
2026-04-21 23:21:23 +0200

    JEI Exclusion zone

--- src/main/java/mcjty/xnet/blocks/wireless/GuiWirelessRouter.java
+++ src/main/java/mcjty/xnet/blocks/wireless/GuiWirelessRouter.java
@@ -1,14 +1,14 @@
 package mcjty.xnet.blocks.wireless;
 
 import mcjty.lib.container.GenericContainer;
-import mcjty.lib.gui.GenericGuiContainer;
 import mcjty.lib.gui.Window;
 import mcjty.xnet.XNet;
+import mcjty.xnet.blocks.generic.GenericXNetGuiContainer;
 import mcjty.xnet.setup.GuiProxy;
 import mcjty.xnet.network.XNetMessages;
 import net.minecraft.util.ResourceLocation;
 
-public class GuiWirelessRouter extends GenericGuiContainer<TileEntityWirelessRouter> {
+public class GuiWirelessRouter extends GenericXNetGuiContainer<TileEntityWirelessRouter> {
 
     public GuiWirelessRouter(TileEntityWirelessRouter router, GenericContainer container) {
         super(XNet.instance, XNetMessages.INSTANCE, router, container, GuiProxy.GUI_MANUAL_XNET, "wireless_router");
